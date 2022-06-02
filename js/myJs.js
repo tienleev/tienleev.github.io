4e358a1f9919a1d3792cc698d52093f35ba2fba0
tienleev <95156045+tienleev@users.noreply.github.com>
2022-06-02 21:19:50 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -2,7 +2,7 @@ const textConfig = {
   text1: "He luu Huỳn!",
   text2: "Anh có điều này muốn hỏi em nhớ phải trả lời thật lòng nhaaa.",
   text3: "Em yêu anh có phải không nào ._.",
-  text4: "Nếu em không trả lời mà thoát ra tức là muốn làm anh tớ rùi đó nha :v",
+  text4: "Nếu em không trả lời mà thoát ra tức là muốn làm vợ anh rùi đó nha :v",
   text5: "Anh mơ à???",
   text6: "Yêu ơi là yêu <3",
   text7: "Lí do em thích anh đi :vvvv",
